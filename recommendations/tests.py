829000a761a435ccd1a50f57ff15e59df101dcc8
RPintre <7090150+RPintre@users.noreply.github.com>
2026-08-04 16:57:01 +0200

    Vectorisation TF-IDF des series

--- recommendations/tests.py
+++ recommendations/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .vectorizer import build_show_vectors, fit_vectorizer
+
+
+class VectorizerTests(TestCase):
+    def setUp(self):
+        self.corpus = [
+            "Un professeur de chimie malade fabrique de la drogue avec un ancien eleve",
+            "Un groupe d'amis se retrouve chaque semaine dans un cafe a New York",
+            "Des enfants affrontent des monstres venus d'un monde parallele",
+        ]
+        self.vectorizer = fit_vectorizer(self.corpus)
+        self.vectors = build_show_vectors(self.vectorizer, self.corpus)
+
+    def test_vectors_match_corpus(self):
+        self.assertEqual(len(self.vectors), len(self.corpus))
