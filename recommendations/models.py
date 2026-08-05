4e01339b6fb2ca771e528cae1c3262b27f7e944c
RPintre <7090150+RPintre@users.noreply.github.com>
2026-08-05 15:57:35 +0200

    Modele DuelChoice

--- recommendations/models.py
+++ recommendations/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+from shows.models import Show
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
@@ -12,4 +14,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"Profil de {self.user.username}"
 
-# TODO: modele DuelChoice (une fois Show pret)
+
+class DuelChoice(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='duel_choices')
+    show_chosen = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='+')
+    show_rejected = models.ForeignKey(Show, on_delete=models.CASCADE, related_name='+')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.show_chosen} > {self.show_rejected}"
